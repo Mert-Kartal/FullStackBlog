6333e4bf27294ab9129c89d3400ffe7cd6f37816
Mert KARTAL <179147337+Mert-Kartal@users.noreply.github.com>
2025-07-16 15:18:38 +0300

    refactor: removed post module to rewrite

--- api/src/post/post.module.ts
+++ api/src/post/post.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
-import { PostRepository } from './post.repository';
 import { PrismaModule } from '../prisma/prisma.module';
-import { SharedModule } from '../shared/shared.module';
-import { TagModule } from '../tag/tag.module';
-import { CategoryModule } from '../category/category.module';
-import { CommentRepository } from './comment/comment.repository';
-import { CommentService } from './comment/comment.service';
 @Module({
-  imports: [PrismaModule, SharedModule, TagModule, CategoryModule],
+  imports: [PrismaModule],
   controllers: [PostController],
-  providers: [PostService, PostRepository, CommentRepository, CommentService],
+  providers: [PostService],
 })
 export class PostModule {}
